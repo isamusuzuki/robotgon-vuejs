98990aa5c463bac703d223744d9a11511cdde437
Isamu Suzuki <isamusuzuki@outlook.com>
2021-09-19 11:28:27 +0900

    initial commit: イニシャルコミット

---
+++ README.md
@@ -0,0 +1,13 @@
+# robotgon-vuejs
+
+ロボットゴン Vue.js 3 勉強
+
+作成日 2021/09/19
+
+## 01. ファイル・フォルダ構成
+
+```text
+--robotgon-vuejs3/
+    |--public/  ... コンパイル後のJavaScriptの置き場
+    `--src/     ... コンパイル前のTypeScriptの置き場
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Avocado</h1>
+  <h2>{{ programVersion }}</h2>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'App',
+    data() {
+      return {
+        programVersion: 'v0000 開発中',
+      };
+    },
+  });
+</script>
