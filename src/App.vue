71fb54599a0fd0cad40424b70417ddb25939eb13
Isamu Suzuki <isamusuzuki@outlook.com>
2021-09-20 21:09:15 +0900

    refactor: 同じコンポーネント２回から違うコンポーネントへ

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
     <section class="section">
         <div class="container">
             <h1 class="title block">Avocado</h1>
-            <counter></counter>
+            <nav class="level">
+                <div class="level-left">
+                    <button class="level-item button is-outlined" @click="decrement">-</button>
+                    <span class="level-item is-size-3" v-text="count" />
+                    <button class="level-item button is-outlined" @click="increment">+</button>
+                </div>
+            </nav>
             <counter></counter>
         </div>
     </section>
@@ -11,7 +17,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Counter from './components/Counter.vue'
+import { useCount } from './hooks/useCount'
 export default defineComponent({
     components: { Counter },
+    setup() {
+        return {
+            ...useCount()
+        }
+    }
 })
 </script>
